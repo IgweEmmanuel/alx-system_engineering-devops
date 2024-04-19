858dd5cbca55f7d917c34c65f382f2114d892ea2
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-19 23:57:36 +0000

    install flask

--- 0x0A-configuration_management/i-install_a_package.pp
+++ 0x0A-configuration_management/i-install_a_package.pp
@@ -1,7 +1,7 @@
 #This is using package resource to install flask
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/bin',
+package { 'flask':
+  ensure         => 'installed',
+  install_options => ['-v', '2.1.0'],
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
