3114c43a8355235fd81708c145c03a4d892cd7de
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-20 08:30:21 +0000

    installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,7 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+exec { 'flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin', '/usr/local/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
 }
