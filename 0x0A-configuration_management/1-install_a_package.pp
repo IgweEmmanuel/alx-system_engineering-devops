c850aceceb4ffa9f3ef3a07be2c8bd414e4a416d
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-20 08:21:16 +0000

    installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,15 @@
 #This manifest uses package resource to install flask
+exec { 'python3-venv':
+  command => 'sudo apt install python3-venv',
+}
+
+exec { 'venv':
+  command => 'python3 -m venv venv',
+}
+
+exec { 'activate':
+  command => 'source venv/bin/activate',
+}
 
 exec { 'flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
