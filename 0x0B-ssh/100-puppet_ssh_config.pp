9d36c4bc187910669faa3c42af4d4d668148a0c3
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-21 18:12:29 +0000

    puppet test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 file_line { 'turn_of_passord_authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '  PasswordAuthenticaton no',
+  line   => '  PasswordAuthentication no',
 }
 
 file_line { 'identify_file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdenfityFile ~/.ssh/school',
+  line   => 'IdentifyFile ~/.ssh/school',
 }
