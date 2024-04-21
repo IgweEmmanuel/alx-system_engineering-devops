285462034d715e82dfed968aacbb6b983d835663
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-21 19:00:13 +0000

    puppet test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #This is using puppet file to configure remote server
 file { 'access without password':
   ensure  => 'present',
-  content => "
+  command => "
 Host ubuntu@18.207.139.77
   IdentityFile ~/.ssh/school
   PasswordAuthentication no
