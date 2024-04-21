5bc92cea40ee6f1decfc2ee7fd0d298e946786b2
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-21 18:14:01 +0000

    puppet test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #This file will create a connection to ubuntu server without password
-file_line { 'turn_of_passord_authentication':
+file_line { 'turn of passord authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '  PasswordAuthentication no',
 }
 
-file_line { 'identify_file':
+file_line { 'identify file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentifyFile ~/.ssh/school',
