bdf310cde935b945eedcf79acab9152f069bbbbe
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-22 18:37:43 +0000

    flask installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 #This manifest uses package resource to install flask
-exec { 'flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'installed_flask':
+  command     => '/usr/bin/pip3 install flask==2.0.2',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  environment => [],
+}
+
+file { '/usr/local/bin/flask':
+  ensure => link,
+  target => '/usr/bin/flask',
 }
